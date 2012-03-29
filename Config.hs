da4d65e530fc69d828b844421128ec08aa730def
Deian Stefan <deian@cs.stanford.edu>
2012-03-29 16:17:00 -0700

    fix base64 bug

--- Config.hs
+++ Config.hs
@@ -3,11 +3,11 @@ module Config ( gitstar_ssh_web_url
               , gitstar_ssh_web_authorization ) where
 
 import qualified Data.ByteString.Char8 as S8
-import qualified Data.ByteString.Base64.URL as URL
+import qualified Data.ByteString.Base64 as B64
 
 gitstar_ssh_web_url :: String
 gitstar_ssh_web_url = "http://localhost:9292/"
 
 gitstar_ssh_web_authorization :: S8.ByteString
 gitstar_ssh_web_authorization = S8.pack "Basic " `S8.append`
-                                URL.encode (S8.pack "gitstar:w00t")
+                                B64.encode (S8.pack "gitstar:w00t")
